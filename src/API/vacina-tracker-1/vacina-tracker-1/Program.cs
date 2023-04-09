17423d65c4bc1a5fabd1e3a566b4877e2f545fe4
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-04-09 19:23:29 -0300

    criação da tabela vacina

--- src/API/vacina-tracker-1/vacina-tracker-1/Program.cs
+++ src/API/vacina-tracker-1/vacina-tracker-1/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using vacina_tracker_1.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//builder.Services.AddControllers()
+    //.AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
