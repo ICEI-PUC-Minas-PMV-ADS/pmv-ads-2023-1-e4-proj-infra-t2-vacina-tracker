6bce56bbd088e9a75fcd502b272d6df6ddd136bd
Nayara <nayarakatlin@yahoo.com.br>
2023-05-12 00:46:59 -0300

    Commit-Nay
    Update tela esqueceu senha

--- src/Web/vacina-tracker/src/componentes/Pages/EsqueceuSenha/EsqueceuSenha.js
+++ src/Web/vacina-tracker/src/componentes/Pages/EsqueceuSenha/EsqueceuSenha.js
@@ -1,10 +1,28 @@
+import Input from '../../Layout/Input/Input'
+import LinkTexto from '../../Layout/LinkTexto/LinkTexto'
+import Botao from '../../Layout/Botao/Botao'
+
 import './EsqueceuSenha.css'
 
 function EsqueceuSenha() {
     return (
-        <div>
-            <p>Teste para as rotas - Esqueceu a Senha</p>
-        </div>
+        <section className='EsqueceuSenha'>
+
+            <h1>Esqueceu sua Senha?</h1>
+            
+            <div className='EsqueceuSenha-input'>
+               <Input type="text" text="E-mail" name="E-mail" placeholder="Digite o E-mail cadastrado" />
+            </div>
+
+            <div className='Submit-cadastrar'>
+               <Botao Type="Submit" text="Recuperar Senha" name="Recuperar Senha" />
+            </div>
+
+            <div>
+               <LinkTexto to="#" text="Não tem Conta? Cadastre-se"/> 
+            </div>
+
+        </section>
     )
 }
 
