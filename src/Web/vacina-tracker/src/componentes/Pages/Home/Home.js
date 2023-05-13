bbd87b9fdc835dd871832165a423d6d49e41072e
Felipe Moreira <90854853+felipe12M@users.noreply.github.com>
2023-05-13 20:58:13 -0300

    Telas Home e Login

--- src/Web/vacina-tracker/src/componentes/Pages/Home/Home.js
+++ src/Web/vacina-tracker/src/componentes/Pages/Home/Home.js
@@ -1,11 +1,24 @@
 import './Home.css'
+import logo from '../../../img/Logo3 dark2.png'
+import imagem from '../../../img/tela_home.svg'
 
 function Home() {
     return (
-        <div>
-            <p>Teste para as rotas - Home</p>
-        </div>
+        <section>
+            <img className='logo' src={logo} alt='Logo Vacina Tracker' />
+            <h1 className='texto-1'>Seu cartão de vacinas onde você estiver</h1>
+            <div className='box'>
+                <img className='imagem' src={imagem} alt='Tela Home' />
+                
+                <h2 className='texto-2'>Usuário</h2>
+                
+                <div className='posicao'>
+                    <button className='btn-1'>Acessar</button>
+                    <button className='btn-2'>Cadastrar</button>
+                </div>
+            </div>
+        </section>
     )
 }
-
 export default Home
+
