90ee92d2149d444ddca1b0f5c05fd3b75bc07206
Edu-DevBR <440744@sga.pucminas.br>
2023-05-14 19:41:47 -0300

    Subindo Link To= para rotas

--- src/Web/vacina-tracker/src/componentes/Pages/MeusMembros/MeusMembros.js
+++ src/Web/vacina-tracker/src/componentes/Pages/MeusMembros/MeusMembros.js
@@ -1,21 +1,26 @@
 import './MeusMembros.css'
 import React from 'react';
 import TabelaDados from './Tabela';
-
-
+import { FaPlusCircle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function MeusMembros() {
     return (
+        <section>
+            <div className='container-membros'>
 
-        <div className='container-membros'>
+                <h1 className='texto-1-membros'>Meus Membros</h1>
 
-            <h1 className='texto-1-membros'>Meus Membros</h1>
-
-            <TabelaDados />
-
-        </div>
-
-    )
+                <TabelaDados />
+                
+                <div className='lista-icones-membros'>
+                <Link to='/MCadastroMembro'>
+                    <FaPlusCircle className='icone' />
+                </Link>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default MeusMembros
