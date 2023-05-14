bacd3ac613f91facc6d0251d24409b5e489f4040
Nayara <nayarakatlin@yahoo.com.br>
2023-05-14 20:33:03 -0300

    Update Nay-Politica de privacidade

--- src/Web/vacina-tracker/src/componentes/Pages/PoliticaTermos/PoliticaTermos.js
+++ src/Web/vacina-tracker/src/componentes/Pages/PoliticaTermos/PoliticaTermos.js
@@ -1,10 +1,24 @@
+import { Container } from 'react-bootstrap'
 import './PoliticaTermos.css'
 
 function PoliticaTermos() {
     return (
-        <div>
-            <p>Teste para as rotas - Política e Termos</p>
-        </div>
+        <section className='politica-termos'>
+            <Container>
+                    <div>
+                        <text className='titulo'>POLÍTICA DE PRIVACIDADE</text>
+                        <text className='sub-titulo'>Na Vacina Tracker (nome fictício), privacidade e segurança são prioridades e nos comprometemos com a transparência do tratamento de dados pessoais dos nossos usuários/clientes. Por isso, esta presente Política de Privacidade estabelece como é feita a coleta, uso e transferência de informações de clientes ou outras pessoas que acessam ou usam nossa aplicação. {"\n"}
+                            Ao utilizar nossos serviços, você entende que coletaremos e usaremos suas informações pessoais nas formas descritas nesta Política, sob as normas da Constituição Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), das normas de Proteção de Dados (LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis. {"\n"}
+                            Dessa forma, a Vacina Tracker LTDA, doravante denominada simplesmente como “Vacina Tracker” (nome fictício), inscrita no CNPJ/MF sob o nº 004.597.592/0001-20 (número fictício), no papel de Controladora de Dados, obriga-se ao disposto na presente Política de Privacidade.</text>
+                    </div>
+
+                    <div>
+                        <text className='titulo'>TERMOS E CONDIÇÕES DE USO</text>
+                        <text className='sub-titulo'>Os serviços do “Vacina Tracker” (nome fictício) são fornecidos pela pessoa jurídica com a seguinte Razão Social/nome: Vacina Tracker LTDA (nome fictício), com nome fantasia “Vacina Tracker” (nome fictício), inscrito no CNPJ/CPF sob o nº 004.597.592/0001-20 (número fictício), titular da propriedade intelectual sobre software, website, aplicativos, conteúdos e demais ativos relacionados à plataforma Vacina Tracker.</text>
+                    </div>
+            </Container>
+
+        </section>
     )
 }
 
