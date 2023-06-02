b48b73a7a3a8cd026770aa11fcfc93b6db1dd51a
Nayara <nayarakatlin@yahoo.com.br>
2023-06-02 02:48:51 -0300

    Update-Nay
    Update Home

--- src/Mobile/vacina-tracker-mobile/src/components/pages/home/Home.js
+++ src/Mobile/vacina-tracker-mobile/src/components/pages/home/Home.js
@@ -1,17 +1,82 @@
 import React from 'react';
+import { Footer } from '../../layout/footer/Footer';
+
 import {
-    Text,
+    SafeAreaView,
     View,
-  } from "react-native";
+    StyleSheet,
+    Image,
+    Text,
+    TouchableOpacity,
+} from "react-native";
 
 
 export default function Home() {
 
     return (
-        <View>
+        <SafeAreaView>
+            <View style={styles.containerHome}>
+                <Image
+                 source={require("../../../assets/logo-dark-600px.png")}
+                 style={{ width: 330, height: 92, marginTop: 60 }}
+                />
 
-            <Text>HOME</Text>
+                <Text style={styles.homeText1}>Seu cartão de vacinas onde você estiver</Text>
 
-        </View>
+                <View>
+
+                    <TouchableOpacity style={styles.btnConectar} onPress={() => console.log('Pressed')}>
+                        <Text style={styles.homeText2}>Conectar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btnCadastrar} onPress={() => console.log('Pressed')}>
+                        <Text style={styles.homeText2}>Cadastrar</Text>
+                    </TouchableOpacity>
+
+                </View>
+                <Footer />
+            </View>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    containerHome: {
+        backgroundColor: "#1005AD",
+        alignItems: "center",
+        padding: 56,
+    },
+    homeText1: {
+        fontSize: 16,
+        color: "#FFFFFF",
+        textAlign: "center",
+        margin: 8,
+    },
+	btnConectar: {
+        backgroundColor: "#13ADC2",
+        marginTop: 250,
+        margin: 40,
+        textAlign: "center",
+        borderRadius: 10,
+        width: 240,
+        height: 60,
+        justifyContent: "center",
+        marginBottom: 15,
+    },
+	btnCadastrar: {
+        backgroundColor: "#9113C2",
+        marginTop: 32,
+        margin: 40, 
+        textAlign: "center",
+        borderRadius: 10, 
+        width: 240,
+        height: 60,
+        justifyContent: "center",
+    },
+    homeText2: {
+        color: "#F5F5F5",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+
+});
