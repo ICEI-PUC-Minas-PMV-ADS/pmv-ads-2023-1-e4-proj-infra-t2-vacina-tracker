ac7244321aaa0ce927f14d57f6016a2f093ad341
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-06-04 18:33:06 -0300

    finalizar header

--- src/Mobile/vacina-tracker-mobile/src/components/layout/header/Header.js
+++ src/Mobile/vacina-tracker-mobile/src/components/layout/header/Header.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { Appbar } from 'react-native-paper';
 import { Image, StyleSheet } from "react-native";
 
-export default function Header() {
-
-    const _goBack = () => console.log('Went back');
-
-    const _handleMore = () => console.log('Shown more');
+export default function Header() {    
 
     return (
         <>
-            <Appbar.Header style={styles.container}>
-                <Appbar.BackAction onPress={_goBack} />                
-                <Image source={require("../../../assets/logo-dark-300px.png")} style={{ width: 220, height: 60, alignItems: 'center' }} />
-                <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+            <Appbar.Header style={styles.container}>                                
+                <Image source={require("../../../assets/logo-dark-300px.png")} style={{ width: 220, height: 60, alignItems: 'center' }} />                
             </Appbar.Header>            
         </>
     );
@@ -22,6 +16,6 @@ export default function Header() {
 const styles = StyleSheet.create({
     container: {        
         backgroundColor: "#1005AD",  
-        justifyContent: "space-between",            
+        justifyContent: "center",            
     },    
 });
