0cca58e743ea033f7f83cc940929b4e440cdf8fb
Edu-DevBR <440744@sga.pucminas.br>
2023-06-04 18:34:37 -0300

    Updade Edu

--- src/Mobile/vacina-tracker-mobile/src/components/pages/perfil/Perfil.js
+++ src/Mobile/vacina-tracker-mobile/src/components/pages/perfil/Perfil.js
@@ -1,156 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, SafeAreaView, ScrollView, Text, TextInput, View, Alert, } from "react-native";
-import { Avatar } from "react-native-paper";
+import { StyleSheet, SafeAreaView, ScrollView, Text, View, Alert, } from "react-native";
+import { Avatar, TextInput } from "react-native-paper";
 import { Footer } from '../../layout/footer/Footer';
 import InputAzul from '../../layout/input/InputAzul';
-
+import * as Keychain from 'react-native-keychain';
 
 export default function Perfil({ navigation }) {
-  // const btnEditarLabel = "EDITAR";
-  // const btnSairLabel = "SAIR";
-  // const btnSalvarLabel = "SALVAR";
-  // const btnCancelarLabel = "CANCELAR";
-  // const btnExcluirLabel = "EXCLUIR";
 
   const [id, setId] = useState(global.id || "");
   const [email, setEmail] = useState(global.email || "");
   const [senha, setSenha] = useState(global.senha || "");
 
-  // const [layoutEdicao, setLayoutEdicao] = useState("");
-  const [edicaoInputs, setEdicaoInputs] = useState(false);
+  const handleSalvarModificacoes = async () => {
+    try {
+      const token = await Keychain.getInternetCredentials('jwt_token');
+    
+      if (!token) {
+        // Redirecionar para a tela de login ou realizar outra ação adequada
+        return;
+      }
+      const response = await fetch(`https://localhost:7134/api/usuarios/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          senha: senha
+        }),
+      });
 
-  // useEffect(() => {
-  //   setId(global.id);
-  //   setEmail(global.email);
-  //   setSenha(global.senha);
-
-  //   setLayoutEdicao(modoEdicao);
-  // }, []);
-
-  // const modoEdicao = (modoEdicao) => {
-  //   setLayoutEdicao(modoEdicao);
-  //   setEdicaoInputs(modoEdicao);
-  //   Botoes();
-  // };
-
-  // const desejaSair = () => {
-  //   Alert.alert(
-  //     "LOGOFF",
-  //     "Tem certeza de que deseja desconectar da sua conta?",
-  //     [
-  //       {
-  //         text: "CANCELAR",
-  //         onPress: () => { },
-  //         style: "cancel",
-  //       },
-  //       {
-  //         text: "DESCONECTAR",
-  //         onPress: () => {
-  //           logoff();
-  //         },
-  //       },
-  //     ]
-  //   );
-  // };
-
-  // const desejaExcluir = (dados) => {
-  //   Alert.alert(
-  //     "EXCLUIR USUÁRIO",
-  //     "Tem certeza de que deseja apagar o seu usuário ? A exclusão será permanente!",
-  //     [
-  //       {
-  //         text: "CANCELAR",
-  //         onPress: () => { },
-  //         style: "cancel",
-  //       },
-  //       {
-  //         text: "EXCLUIR",
-  //         onPress: () => {
-  //           excluirUsuario(dados);
-  //           sucessoDelete();
-  //           setTimeout(logoff, 500);
-  //         },
-  //       },
-  //     ]
-  //   );
-  // };
-
-  // const excluirUsuario = (dados) => {
-  //   if (dados === true) {
-  //     SQLExecutor.deleteUsuario(id);
-  //   }
-  // };
-
-  // const sucessoDelete = () => {
-  //   showMessage({
-  //     message: "Usuário excluido com sucesso!",
-  //     type: "success",
-  //   });
-  // };
-
-  // function logoff() {
-  //   limparTela();
-  //   limparGlobais();
-  //   redirecionaTela();
-  // }
-
-  // const atualizarUsuario = (dados) => {
-  //   if (dados === true) {
-  //     let usuario = {
-  //       id: id,
-  //       email: email,
-  //       senha: senha,
-  //     };
-
-  //     SQLExecutor.updateUsuario(usuario);
-
-  //     sucessoUpdate();
-  //   }
-  // };
-
-  // const sucessoUpdate = () => {
-  //   showMessage({
-  //     message: "Usuário atualizado com sucesso!",
-  //     type: "success",
-  //   });
-  // };
-
-  // const limparTela = () => {
-  //   setId("");
-  //   setEmail("");
-  //   setSenha("");
-  // };
-
-  // const limparGlobais = () => {
-  //   global.id = "";
-  //   global.email = "";
-  //   global.senha = "";
-  //   global.usuarioLogado = false;
-  // };
-
-  // const redirecionaTela = () => {
-  //   navigation.push("Login");
-  // };
-
-  // const Botoes = () => {
-  //   return (
-  //     <View>
-  //       {layoutEdicao ? (
-  //         <View>
-
-  //         </View>
-  //       ) : (
-  //         <View>
-
-  //         </View>
-  //       )}
-  //     </View>
-  //   );
-  // };
+      if (response.ok) {
+        Alert.alert("Sucesso", "As modificações foram salvas com sucesso.");
+      } else {
+        Alert.alert("Erro", "Não foi possível salvar as modificações.");
+      }
+    } catch (error) {
+      Alert.alert("Erro", "Ocorreu um erro ao salvar as modificações.");
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
+
+        <Text style={styles.text}>PERFIL</Text>
 
         <Avatar.Image
           source={require("../../../../src/assets/meu-perfil.png")}
@@ -158,22 +52,23 @@ export default function Perfil({ navigation }) {
           style={styles.avatar}
         />
 
-        <Text style={styles.text2}>Email </Text>
         <TextInput
-          style={styles.textInput}
+          label="E-mail"
           value={email !== null ? email : ""}
-          onChangeText={setEmail}
-          editable={edicaoInputs}
+          keyboardType="email-address"
+          onChangeText={text => setEmail(text)}
+          style={styles.inputText}
         />
 
-        <Text style={styles.text2}>Senha </Text>
         <TextInput
-          style={styles.textInput}
+          label="Senha"
           value={senha !== null ? senha : ""}
-          onChangeText={setSenha}
-          editable={edicaoInputs}
+          keyboardType="senha"
+          onChangeText={text => setSenha(text)}
+          style={styles.inputText}
         />
-        <InputAzul text="Salvar Modificações" />
+
+        <InputAzul text="Salvar Modificações" onPress={handleSalvarModificacoes}/>
 
       </ScrollView>
       <Footer />
@@ -183,18 +78,14 @@ export default function Perfil({ navigation }) {
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 16,
-    textAlign: "center",
-    padding: 5,
-    color: "#FFFFFF"
-  },
-  text2: {
+    backgroundColor: "#1005AD",
     fontWeight: "bold",
-    fontSize: 16,
-    textAlign: "justify",
-    padding: 8,
-    color: "#FFFFFF"
+    fontSize: 36,
+    color: "#9113C2",
+    textAlign: "center",
+    marginTop: 30,
   },
+
   container: {
     flex: 1,
     alignItems: "center",
@@ -206,16 +97,13 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: "#1005AD",
   },
-  textInput: {
-    backgroundColor: "#FFFFFF",
-    width: "100%",
-    marginBottom: 15,
-    color: "#222",
-    fontSize: 16,
-    borderRadius: 20,
-    padding: 7,
-    margin: "auto",
+  inputText: {
+    backgroundColor: "#1005AD",
+    color: "#FFFFFF",
+    marginTop: 24,
+    margin: 16,
   },
+
   avatar: {
     backgroundColor: "#FFFFFF",
     alignSelf: 'center',
