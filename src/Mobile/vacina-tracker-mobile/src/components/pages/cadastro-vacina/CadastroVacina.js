50401067e7d47965e92214fbefe77a8b0e3ae505
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-06-04 00:50:57 -0300

    tela cadastrar vacina

--- src/Mobile/vacina-tracker-mobile/src/components/pages/cadastro-vacina/CadastroVacina.js
+++ src/Mobile/vacina-tracker-mobile/src/components/pages/cadastro-vacina/CadastroVacina.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { TextInput } from 'react-native-paper';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
 import { Footer } from '../../layout/footer/Footer';
 
@@ -12,40 +13,47 @@ export default function CadastroVacina() {
     const [dataProxAplicacao, setDataProxAplicacao] = React.useState("");
 
     return (
-        <View style={styles.containerCadastro}>
+        <View style={styles.containerCadastroVacina}>
 
             <Text style={styles.cadastroVacinaText1}>Cadastrar Vacina</Text>
 
-            <TextInput
-                label="Nome da Vacina"
-                value={nome} 
-                onChangeText={text => setNome(text)}
-                style={styles.input1}
-            />
+            <View style={styles.boxCadastroVacina}>
+                <TextInput
+                    label="Nome da Vacina"
+                    value={nome}
+                    onChangeText={text => setNome(text)}
+                    style={styles.input1}
+                />
 
-            <TextInput
-                label="Data da Aplicação"
-                value={dataAplicacao}
-                keyboardType="date"
-                onChangeText={text => setDataAplicacao(text)}
-                style={styles.input1}
-            />
+                <TextInput
+                    label="Data da Aplicação"
+                    value={dataAplicacao}
+                    keyboardType="date"
+                    onChangeText={text => setDataAplicacao(text)}
+                    style={styles.input1}
+                />
 
-            <TextInput
-                label="Dose"
-                value={dose}        
-                onChangeText={text => setDose(text)}
-                style={styles.input1}
-            />
+                <TextInput
+                    label="Dose"
+                    value={dose}
+                    onChangeText={text => setDose(text)}
+                    style={styles.input1}
+                />
 
-            <TextInput
-                label="Data da Próxima Aplicação"
-                value={dataProxAplicacao}
-                keyboardType="date"
-                dataDetectorTypes="calendarEvent"
-                onChangeText={text => setDataProxAplicacao(text)}
-                style={styles.input1}
-            />
+                <TextInput
+                    label="Data da Próxima Aplicação"
+                    value={dataProxAplicacao}
+                    keyboardType="date"
+                    dataDetectorTypes="calendarEvent"
+                    onChangeText={text => setDataProxAplicacao(text)}
+                    style={styles.input1}
+                />
+
+                <View style={styles.iconesCadastroV}>
+                    <MaterialCommunityIcons name="content-save" size={32} color="#13ADC2" />
+                    <AntDesign name="pluscircle" size={32} color="#13ADC2" />
+                </View>
+            </View>
 
             <Footer />
 
@@ -54,7 +62,7 @@ export default function CadastroVacina() {
 }
 
 const styles = StyleSheet.create({
-    containerCadastro: {
+    containerCadastroVacina: {
         backgroundColor: "#1005AD",
     },
     cadastroVacinaText1: {
@@ -65,11 +73,30 @@ const styles = StyleSheet.create({
         textAlign: "start",
         paddingHorizontal: 16,
         marginTop: 24,
+        marginBottom: 8,
+    },
+    boxCadastroVacina: {        
+        backgroundColor: "#9113C2",
+        opacity: 0.5,
+        maxWidth: 500,
+        maxHeight: 500,
+        borderRadius: 10,
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        margin: 8,
     },
     input1: {
-        backgroundColor: "#1005AD",
-        color: "#FFFFFF",
+        backgroundColor: "#9113C2",
+        fontWeight: "bold",
+        fontColor: "#FFFFFF",
         marginTop: 24,
         margin: 16,
+    },
+    iconesCadastroV: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginTop: 40,
     },
 });
